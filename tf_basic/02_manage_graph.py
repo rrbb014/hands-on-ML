4939948234ad1f779c4077173be665634ef2ec15
rrbb014 <gutssoul1@gmail.com>
2018-06-19 11:34:53 +0900

    Add comment

--- tf_basic/02_manage_graph.py
+++ tf_basic/02_manage_graph.py
@@ -1,5 +1,8 @@
 # Managing TF graph p.301
 # 여러 계산 그래프를 만들때
+# 만약, Jupyter 나 python shell 에서 실행할땐 default graph에 여러 노드가 중복 실행되는 경우가 있으니,
+# tf.reset_default_graph() 를 실행해주면 초기화되서 유용하다
+
 import tensorflow as tf
 
 x1 = tf.Variable(1)
