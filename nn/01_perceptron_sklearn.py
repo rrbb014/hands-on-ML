63abc335d1bd934e26645ff1c570eab46ae88601
rrbb014 <gutssoul1@gmail.com>
2018-06-22 23:06:40 +0900

    Create: sklearn-Perceptron

---
+++ nn/01_perceptron_sklearn.py
@@ -0,0 +1,12 @@
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.linear_model import Perceptron
+
+iris = load_iris()
+X = iris.data[:, (2,3)]                # 꽃잎의 길이, 너비
+y = (iris.target == 0).astype(np.int)  # Setosa 인가?
+
+per_clf = Perceptron(random_state=42)
+per_clf.fit(X)
+
+y_pred = per_clf.predict([[2, 0.5]]) 
