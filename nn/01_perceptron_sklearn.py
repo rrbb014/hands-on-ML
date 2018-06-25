5f69cf2e57a16091bc072da7a52906f000789c60
rrbb014 <gutssoul1@gmail.com>
2018-06-25 17:08:41 +0900

    Create: perceptron using sklearn package

--- nn/01_perceptron_sklearn.py
+++ nn/01_perceptron_sklearn.py
@@ -1,12 +1,14 @@
 import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
 
 iris = load_iris()
 X = iris.data[:, (2,3)]                # 꽃잎의 길이, 너비
 y = (iris.target == 0).astype(np.int)  # Setosa 인가?
 
 per_clf = Perceptron(random_state=42)
-per_clf.fit(X)
+per_clf.fit(X, y)
 
 y_pred = per_clf.predict([[2, 0.5]]) 
+print(y_pred)
